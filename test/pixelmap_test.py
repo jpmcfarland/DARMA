3ed271c29a1bad5df643f2adcfd1e8e8c35dd7fd
J. P. Mc Farland <j.p.mc.farland@rug.nl>
2016-08-10 21:09:27 +0000

    - updated DARMA pixelmap unittest

--- test/pixelmap_test.py
+++ test/pixelmap_test.py
@@ -4,63 +4,140 @@ Unit test for DARMA pixelmap
 
 __version__ = '@(#)$Revision$'
 
-from ..common import DARMAError
-from .. import image, image_generator
+from ..common import DARMAError, unicode
+from ..pixelmap import pixelmap
+from .common_test import fits, Array
 
-import unittest, os
+import unittest, os, collections
 
-def build_test_data():
+SINGLE1 = 'SEF1.fits'
+SINGLE2 = 'SEF2.fits'
+#SINGLES = [SINGLE1, SINGLE2]
+SINGLES = [SINGLE1]
+MULTI1 = 'MEF1.fits'
+MULTI2 = 'MEF2.fits'
+#MULTIS = [MULTI1, MULTI2]
+MULTIS = [MULTI1]
+CUBE1 = 'CUBE1.fits'
+CUBE2 = 'CUBE2.fits'
+#CUBES = [CUBE1, CUBE2]
+CUBES = [CUBE1]
+EMPTY1 = 'EMPTY1.fits'
+EMPTY2 = 'EMPTY2.fits'
+#EMPTYS = [EMPTY1, EMPTY2]
+EMPTYS = [EMPTY1]
+ZERO1 = 'ZERO1.fits'
+ZERO2 = 'ZERO2.fits'
+#ZEROS = [ZERO1, ZERO2]
+ZEROS = [ZERO1]
+ONE1 = 'ONE1.fits'
+ONE2 = 'ONE2.fits'
+#ONES = [ONE1, ONE2]
+ONES = [ONE1]
+FILENAMES = SINGLES+MULTIS+EMPTYS+CUBES+ZEROS+ONES
+
+def build_test_data_sef():
+    '''
+       This function builds SEF files to be used in testing
+    '''
+    data = Array.random.normal(1.0, 0.5, (32,16)).astype('float32')
+    for filename in SINGLES:
+        fits.PrimaryHDU(data=data).writeto(filename, output_verify='silentfix', clobber=True)
+
+def build_test_data_mef():
+    '''
+       This function builds MEF files to be used in testing
+    '''
+    data = Array.random.normal(1.0, 0.5, (32,16)).astype('float32')
+    for filename in MULTIS:
+        hdu0 = fits.PrimaryHDU()
+        hdu1 = fits.ImageHDU(data=data)
+        update_header(hdu1.header, 'EXTNAME', 'EXT1')
+        hdu2 = fits.ImageHDU(data=data)
+        update_header(hdu2.header, 'EXTNAME', 'EXT2')
+        hdu3 = fits.ImageHDU(data=data)
+        update_header(hdu3.header, 'EXTNAME', 'EXT3')
+        hdus = fits.HDUList([hdu0, hdu1, hdu2, hdu3])
+        hdus.writeto(filename, output_verify='silentfix', clobber=True)
+        hdus.close()
+
+def build_test_data_empty():
+    '''
+       This function builds dataless files to be used in testing
+    '''
+    for filename in EMPTYS:
+        fits.PrimaryHDU().writeto(filename, output_verify='silentfix', clobber=True)
+
+def build_test_data_zero():
+    '''
+       This function builds SEF files with all zero data array to be
+       used in testing
+    '''
+    data = Array.zeros((32, 16), dtype='int32')
+    for filename in ZEROS:
+        fits.PrimaryHDU(data=data).writeto(filename, output_verify='silentfix', clobber=True)
+
+def build_test_data_one():
+    '''
+       This function builds SEF files with all one data array to be
+       used in testing
+    '''
+    data = Array.ones((32, 16), dtype='int32')
+    for filename in ONES:
+        fits.PrimaryHDU(data=data).writeto(filename, output_verify='silentfix', clobber=True)
+
+def delete_test_data():
+    '''
+       This function deletes fits files used in testing
+    '''
+    for filename in FILENAMES:
+        if os.path.exists(filename):
+            os.remove(filename)
+
+########################################################################
+#                                                                      #
+#                        pixelmap load tests                           #
+#                                                                      #
+########################################################################
+
+class pixelmap_load_error_test(unittest.TestCase):
 
     '''
-       This function builds fits files to be used in testing
-
-       The function returns a list of names of generated files
+       Do pixelmaps loaded from bogus sources raise errors?
     '''
 
-    name_list = []
+    def setUp(self):
+        build_test_data_zero()
+        build_test_data_one()
 
-    print('Generating testdata...')
+    def tearDown(self):
+        delete_test_data()
 
-    ig = image_generator.image_generator(128, 128)
-    for i in range(1,3):
-        name = 'IM%02d.fits' % i
-        ima = ig.generate_random_gauss(0.5*i, 1.0*i)
-        ima.save(name)
-        name_list.append(name)
+    def test_load_error(self):
+        print(self.__class__.__name__)
+        self.assertRaises(DARMAError, pixelmap, filename='Unknown.fits')
+        msk = pixelmap(filename=ZERO1, extension=1)
+        self.assertRaises(DARMAError, msk.load)
 
-    return name_list
-
-def delete_test_data(name_list):
+class pixelmap_load_empty_test(unittest.TestCase):
 
     '''
-       Clean up the mess
+       Are pixelmaps loaded without data empty?
     '''
 
-    for filename in name_list:
-        os.remove(filename)
+    def setUp(self):
+        build_test_data_empty()
 
+    def tearDown(self):
+        delete_test_data()
 
-class pixelmap_tests(unittest.TestCase):
-
-    def test_make_map(self):
-        ima = image.image('IM01.fits')
-        pm = ima.thresh_to_pixmap(0.8, 1.2)
-        pm.dump_pixelmap('pm.fits')
-        self.assertTrue(os.path.exists('pm.fits'))
-        os.remove('pm.fits')
-
-    def test_inspect_map(self):
-        ima = image.image('IM01.fits')
-        pm = ima.thresh_to_pixmap(0.8, 1.2)
-        self.assertEqual(pm.xsize(), ima.xsize())
-        self.assertEqual(pm.ysize(), ima.ysize())
-
-test_suite = unittest.TestSuite()
-test_suite.addTest(unittest.makeSuite(pixelmap_tests))
+    def test_load_empty(self):
+        print(self.__class__.__name__)
+        msk = pixelmap()
+        self.assertIsNone(msk.data, msg='data array from empty pixelmap not None')
+        msk = pixelmap(filename=EMPTY1)
+        self.assertIsNone(msk.data, msg='data array from dataless FITS not None')
 
 if __name__ == '__main__':
-    name_list = build_test_data()
-    test_runner = unittest.TextTestRunner()
-    test_runner.run(test_suite)
-    delete_test_data(name_list)
+    unittest.main()
 
